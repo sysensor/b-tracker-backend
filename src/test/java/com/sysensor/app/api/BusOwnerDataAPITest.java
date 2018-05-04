b1d661808a84b53930fffd50af372be3c060dd7b
Dinuka Malalanayake <dinuka.malalanayake@gmail.com>
2018-05-04 12:36:38 -0500

    Complete the API test for BusOwner with create and delete operations

--- src/test/java/com/sysensor/app/api/BusOwnerDataAPITest.java
+++ src/test/java/com/sysensor/app/api/BusOwnerDataAPITest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.sysensor.app.config.APIConfig;
 import com.sysensor.app.model.BusOwner;
 import org.hamcrest.collection.IsCollectionWithSize;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,6 +30,8 @@ public class BusOwnerDataAPITest {
 
     @Autowired
     private MockMvc mock;
+
+    Gson JSON = new Gson();
 
     @Test
     public void busOwnerDataAPIShouldReturnThreeOwners() throws Exception {
@@ -63,9 +67,8 @@ public class BusOwnerDataAPITest {
         busOwner.setPhone("7656789826");
         busOwner.setUsername("malinda");
         busOwner.setPassword("Test123");
-        Gson json = new Gson();
-        String busOwnerJson = json.toJson(busOwner);
 
+        String busOwnerJson = JSON.toJson(busOwner);
 
         this.mock.perform(post(APIConfig.DATA_API_BUS_OWNER)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -79,6 +82,52 @@ public class BusOwnerDataAPITest {
                 .andExpect(jsonPath(".phone").value("7656789826"))
                 .andExpect(jsonPath(".username").value("malinda"))
                 .andExpect(jsonPath(".password").doesNotExist());
+
+    }
+
+    @Test
+    public void busOwnerDataAPIShouldDeleteTheBusOwners() throws Exception {
+
+        String userName = "admin";
+        String userPassword = "admin";
+        String userAuthorization = "Basic " + Base64.getEncoder().encodeToString((userName + ":" + userPassword).getBytes());
+
+        BusOwner busOwner = new BusOwner();
+        busOwner.setName("Malinda");
+        busOwner.setAddress("Madapatha");
+        busOwner.setPhone("7656789826");
+        busOwner.setUsername("malinda");
+        busOwner.setPassword("Test123");
+
+        String busOwnerJson = JSON.toJson(busOwner);
+        List<String> selfList = new ArrayList<>();
+
+        this.mock.perform(post(APIConfig.DATA_API_BUS_OWNER)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", userAuthorization)
+                .content(busOwnerJson)
+        ).andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath(".name").value("Malinda"))
+                .andExpect(jsonPath(".address").value("Madapatha"))
+                .andExpect(jsonPath(".phone").value("7656789826"))
+                .andExpect(jsonPath(".username").value("malinda"))
+                .andExpect(jsonPath(".password").doesNotExist())
+                .andExpect(jsonPath("._links.self.href").isNotEmpty())
+                .andDo((result) -> {
+                    JSONObject json = new JSONObject(result.getResponse().getContentAsString());
+                    //Capture the returned SELF URL for the delete operation
+                    selfList.add(json.getJSONObject("_links").getJSONObject("self").getString("href"));
+                });
+
+
+        this.mock.perform(delete(selfList.get(0))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", userAuthorization)
+        ).andDo(print())
+                .andExpect(status().isNoContent());
 
     }
 
