9ff2c2f0f72b43d419e49e48aaebbe15214c330a
Dinuka Malalanayake <dinuka.malalanayake@gmail.com>
2018-05-11 15:41:32 -0500

    Fix the issues from FindBug

--- src/test/java/com/sysensor/app/TestConst.java
+++ src/test/java/com/sysensor/app/TestConst.java
@@ -2,17 +2,17 @@ package com.sysensor.app;
 
 public class TestConst {
 
-    public static String BUS_OWNER_ONE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba541";
-    public static String BUS_OWNER_TWO_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba542";
-    public static String BUS_OWNER_THREE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba543";
+    public static final String BUS_OWNER_ONE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba541";
+    public static final String BUS_OWNER_TWO_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba542";
+    public static final String BUS_OWNER_THREE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba543";
 
-    public static String BUS_ONE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077b4008";
-    public static String BUS_TWO_UUID ="bf4cb437-4881-450b-b0fa-4cfe077b4009";
-    public static String BUS_THREE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077b4010";
+    public static final String BUS_ONE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077b4008";
+    public static final String BUS_TWO_UUID ="bf4cb437-4881-450b-b0fa-4cfe077b4009";
+    public static final String BUS_THREE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077b4010";
 
-    public static String BUS_ROUTE_ONE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba547";
-    public static String BUS_ROUTE_TWO_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba548";
+    public static final String BUS_ROUTE_ONE_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba547";
+    public static final String BUS_ROUTE_TWO_UUID ="bf4cb437-4881-450b-b0fa-4cfe077ba548";
 
-    public static String TICKET_ONE_UUID ="77243dc3-d162-4aa4-bcb0-6eb741173a01";
-    public static String TICKET_TWO_UUID ="77243dc3-d162-4aa4-bcb0-6eb741173a02";
+    public static final String TICKET_ONE_UUID ="77243dc3-d162-4aa4-bcb0-6eb741173a01";
+    public static final String TICKET_TWO_UUID ="77243dc3-d162-4aa4-bcb0-6eb741173a02";
 }
